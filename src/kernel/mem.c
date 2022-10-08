cfd64360c934e1641e37c82ebc6d7ddb3d70f33d
qsliu <qsliu2017@outlook.com>
2022-10-08 22:20:44 +0800

    impl slab

--- src/kernel/mem.c
+++ src/kernel/mem.c
@@ -3,22 +3,25 @@
 #include <driver/memlayout.h>
 #include <kernel/init.h>
 #include <kernel/mem.h>
+#include <kernel/slab.h>
 
 RefCount alloc_page_cnt;
 
 static void kinit_page();
-static void kinit_block();
 
 define_early_init(alloc_page_cnt)
 {
     init_rc(&alloc_page_cnt);
     kinit_page();
-    kinit_block();
 }
 
 typedef struct Page
 {
-    QueueNode freequeue;
+    union
+    {
+        QueueNode freequeue; /* free page */
+        char mem[0];         /* allocated page */
+    };
 } Page;
 
 static QueueNode *freepages;
@@ -36,8 +39,8 @@ void kinit_page()
 void *kalloc_page()
 {
     auto freepage = container_of(fetch_from_queue(&freepages), Page, freequeue);
-    ASSERT(freepage != NULL);
-    return freepage;
+    // ASSERT(freepage != NULL);
+    return freepage->mem;
 }
 
 void kfree_page(void *p)
@@ -46,120 +49,12 @@ void kfree_page(void *p)
     add_to_queue(&freepages, &page->freequeue);
 }
 
-typedef struct Block
-{
-    usize order; // this field saved for both free and alloc block
-    ListNode list;
-} Block;
-
-static INLINE Block *block_container_of(ListNode *p)
-{
-    return container_of(p, Block, list);
-}
-
-static Block *detach_block(usize order);
-static void merge_block(Block *b);
-
-const usize BLOCK_POWER_BASE = 5, BLOCK_SIZE_LOG2_MAX = 12;
-
-#define NCPU 4
-
-static Block dummy_block[NCPU][8];
-
-void kinit_block()
-{
-    for (auto cpu = 0; cpu < NCPU; cpu++)
-        for (auto order = 0; order < 8; order++)
-        {
-            dummy_block[cpu][order].order = order;
-            init_list_node(&dummy_block[cpu][order].list);
-        }
-}
-
-usize log2ceil(u64 n)
-{
-    usize i = 0;
-    n--;
-    while (n >> i)
-        i++;
-    return i + 1;
-}
-
 void *kalloc(isize size)
 {
-    auto order = MAX(log2ceil(size + sizeof(Block) - sizeof(ListNode)), BLOCK_POWER_BASE) - BLOCK_POWER_BASE;
-    Block *alloc = detach_block(order);
-    return (void *)&alloc->list;
-}
-
-Block *detach_block(usize order)
-{
-    if (_empty_list(&dummy_block[cpuid()][order].list))
-    {
-        if (order < BLOCK_SIZE_LOG2_MAX - BLOCK_POWER_BASE)
-        {
-            Block *first_block = detach_block(order + 1);
-            Block *second_block = (Block *)((u64)first_block + (1 << (order + BLOCK_POWER_BASE)));
-            first_block->order = second_block->order = order;
-            init_list_node(&first_block->list);
-            init_list_node(&second_block->list);
-            _merge_list(&dummy_block[cpuid()][order].list, &second_block->list);
-            return first_block;
-        }
-        else
-        {
-            Block *block = (Block *)kalloc_page();
-            block->order = order;
-            init_list_node(&block->list);
-            return block;
-        }
-    }
-    else
-    {
-        auto alloc = dummy_block[cpuid()][order].list.next;
-        _detach_from_list(alloc);
-        return block_container_of(alloc);
-    }
+    return _kmalloc((usize)size);
 }
 
 void kfree(void *p)
 {
-    Block *block = block_container_of(p);
-    init_list_node(&block->list);
-    merge_block(block);
-}
-
-void merge_block(Block *b)
-{
-    auto order = b->order;
-    if (order < BLOCK_SIZE_LOG2_MAX - BLOCK_POWER_BASE)
-    {
-        _for_in_list(list_ptr, &dummy_block[cpuid()][order].list)
-        {
-            Block *a = block_container_of(list_ptr);
-            u64 a_addr = (u64)a, b_addr = (u64)b;
-            if ((a_addr >> (order + BLOCK_POWER_BASE + 1)) == ((b_addr >> (order + BLOCK_POWER_BASE + 1))))
-            {
-                _detach_from_list(list_ptr);
-                Block *block = (a_addr >> (order + BLOCK_POWER_BASE)) & 1 ? b : a;
-                block->order++;
-                init_list_node(&block->list);
-                return merge_block(block);
-            }
-        }
-    }
-    else
-    {
-        int cnt = 0;
-        _for_in_list(list_ptr, &dummy_block[cpuid()][order].list)
-        {
-            cnt++;
-        }
-        if (cnt > 0)
-        {
-            return kfree_page(b);
-        }
-    }
-
-    _merge_list(&dummy_block[cpuid()][order].list, &b->list);
+    _kfree(p);
 }
---
+++ src/kernel/slab.c
@@ -0,0 +1,180 @@
+#include <common/string.h>
+#include <kernel/mem.h>
+#include <kernel/init.h>
+#include <kernel/slab.h>
+
+static KCache cache_cache;
+
+static void init_sizes_cache();
+
+define_init(cache)
+{
+  cache_cache.objsize = round_up(sizeof(KCache), 8);
+  cache_cache.num = (PAGE_SIZE - sizeof(Slab)) / cache_cache.objsize;
+  init_spinlock(&cache_cache.spinlock);
+
+  init_list_node(&cache_cache.slabs_free);
+  init_list_node(&cache_cache.slabs_full);
+  init_list_node(&cache_cache.slabs_partial);
+
+  init_sizes_cache();
+}
+
+KCache *kcache_create(usize size)
+{
+  KCache *cachep = (KCache *)kcache_alloc(&cache_cache);
+  cachep->objsize = round_up(size, 8); /* align to word size */
+  cachep->num = (PAGE_SIZE - sizeof(Slab)) / cachep->objsize;
+  init_spinlock(&cachep->spinlock);
+
+  init_list_node(&cachep->slabs_free);
+  init_list_node(&cachep->slabs_full);
+  init_list_node(&cachep->slabs_partial);
+
+  return cachep;
+}
+
+static inline u32 *next_freeobj(KCache *cachep, Slab *slabp, int index)
+{
+  return (u32 *)(index * cachep->objsize + round_up((u64)slabp->mem, 8));
+}
+
+const u32 BUFCTL_END = 0xffffffff;
+
+void *kcache_alloc(KCache *cachep)
+{
+  setup_checker(kcache_alloc);
+  acquire_spinlock(kcache_alloc, &cachep->spinlock);
+  if (_empty_list(&cachep->slabs_partial))
+  {
+    if (_empty_list(&cachep->slabs_free))
+    {
+      void *pagep = kalloc_page();
+      Slab *slabp = (Slab *)pagep;
+      slabp->cache = cachep;
+      _insert_into_list(&cachep->slabs_free, &slabp->list);
+      slabp->inuse = 0;
+      slabp->free = 0;
+      for (u32 i = 0; i < cachep->num; i++)
+      {
+        *next_freeobj(cachep, slabp, i) = i + 1;
+      }
+      *next_freeobj(cachep, slabp, cachep->num - 1) = BUFCTL_END;
+    }
+    Slab *slabp = container_of(cachep->slabs_free.next, Slab, list);
+    _detach_from_list(&slabp->list);
+    _merge_list(&cachep->slabs_partial, &slabp->list);
+  }
+  Slab *slabp = container_of(cachep->slabs_partial.next, Slab, list);
+
+  void *alloc = (void *)next_freeobj(cachep, slabp, slabp->free);
+  slabp->free = *(u32 *)alloc;
+
+  if (++slabp->inuse >= cachep->num)
+  {
+    _detach_from_list(&slabp->list);
+    _merge_list(&cachep->slabs_full, &slabp->list);
+  }
+  release_spinlock(kcache_alloc, &cachep->spinlock);
+
+  return alloc;
+}
+
+void kcache_free(KCache *cachep, void *objp)
+{
+  setup_checker(kcache_free);
+  acquire_spinlock(kcache_free, &cachep->spinlock);
+  u64 page_base = PAGE_BASE((u64)objp);
+
+  ListNode *slabs_lists[] = {&cachep->slabs_full, &cachep->slabs_partial};
+  for (usize i = 0; i < sizeof(slabs_lists) / sizeof(slabs_lists[0]); i++)
+  {
+    ListNode *slabs_list = slabs_lists[i];
+    _for_in_list(ptr, slabs_list)
+    {
+      Slab *slabp = container_of(ptr, Slab, list);
+      if (page_base == PAGE_BASE((u64)slabp->mem))
+      {
+        int index = ((u64)objp - (u64)slabp->mem) / cachep->objsize;
+        *next_freeobj(cachep, slabp, index) = slabp->free;
+        slabp->free = index;
+        _detach_from_list(&slabp->list);
+        if (--slabp->inuse > 0)
+        {
+          _merge_list(&cachep->slabs_partial, &slabp->list);
+        }
+        else
+        {
+          _merge_list(&cachep->slabs_free, &slabp->list);
+        }
+        goto done;
+      }
+    }
+  }
+
+done:
+  release_spinlock(kcache_free, &cachep->spinlock);
+}
+
+int kcache_destroy(KCache *cachep)
+{
+  _acquire_spinlock(&cachep->spinlock);
+  ListNode *slabs_lists[] = {&cachep->slabs_full, &cachep->slabs_partial, &cachep->slabs_free};
+
+  for (usize i = 0; i < sizeof(slabs_lists) / sizeof(slabs_lists[0]); i++)
+  {
+    ListNode *slabs_list = slabs_lists[i];
+    _for_in_list(ptr, slabs_list)
+    {
+      Slab *slabp = container_of(ptr, Slab, list);
+      _detach_from_list(&slabp->list);
+      kfree_page((void *)slabp);
+    }
+  }
+
+  kcache_free(&cache_cache, (void *)cachep);
+
+  return 0;
+}
+
+typedef struct SizesCache
+{
+  usize size;
+  KCache *cachep;
+} SizesCache;
+
+static SizesCache cache_sizes[] = {
+    {32, NULL},
+    {64, NULL},
+    {128, NULL},
+    {256, NULL},
+    {512, NULL},
+    {1024, NULL},
+    {2048, NULL},
+};
+
+void init_sizes_cache()
+{
+  for (usize i = 0; i < sizeof(cache_sizes) / sizeof(cache_sizes[0]); i++)
+  {
+    cache_sizes[i].cachep = kcache_create(cache_sizes[i].size);
+  }
+}
+
+void *_kmalloc(usize size)
+{
+  for (usize i = 0; i < sizeof(cache_sizes) / sizeof(cache_sizes[0]); i++)
+  {
+    if (cache_sizes[i].size >= size)
+    {
+      return kcache_alloc(cache_sizes[i].cachep);
+    }
+  }
+  return NULL;
+}
+
+void _kfree(void *objp)
+{
+  Slab *slabp = (void *)PAGE_BASE((u64)objp);
+  kcache_free(slabp->cache, objp);
+}
