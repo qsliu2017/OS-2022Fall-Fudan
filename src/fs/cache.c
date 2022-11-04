053048a5aa83898a18bba175ff89d9d62dec66cd
qsliu <qsliu2017@outlook.com>
2022-11-04 16:36:16 +0800

    format

--- src/fs/cache.c
+++ src/fs/cache.c
@@ -5,40 +5,46 @@
 #include <kernel/printk.h>
 #include <kernel/proc.h>
 
-static const SuperBlock* sblock;
-static const BlockDevice* device;
+static const SuperBlock *sblock;
+static const BlockDevice *device;
 
-static SpinLock lock;     // protects block cache.
-static ListNode head;     // the list of all allocated in-memory block.
-static LogHeader header;  // in-memory copy of log header block.
+static SpinLock lock;    // protects block cache.
+static ListNode head;    // the list of all allocated in-memory block.
+static LogHeader header; // in-memory copy of log header block.
+
 
 // hint: you may need some other variables. Just add them here.
-struct LOG {
-    /* data */
+struct LOG
+{
 } log;
 
 // read the content from disk.
-static INLINE void device_read(Block* block) {
+static INLINE void device_read(Block *block)
+{
     device->read(block->block_no, block->data);
 }
 
 // write the content back to disk.
-static INLINE void device_write(Block* block) {
+static INLINE void device_write(Block *block)
+{
     device->write(block->block_no, block->data);
 }
 
 // read log header from disk.
-static INLINE void read_header() {
-    device->read(sblock->log_start, (u8*)&header);
+static INLINE void read_header()
+{
+    device->read(sblock->log_start, (u8 *)&header);
 }
 
 // write log header back to disk.
-static INLINE void write_header() {
-    device->write(sblock->log_start, (u8*)&header);
+static INLINE void write_header()
+{
+    device->write(sblock->log_start, (u8 *)&header);
 }
 
 // initialize a block struct.
-static void init_block(Block* block) {
+static void init_block(Block *block)
+{
     block->block_no = 0;
     init_list_node(&block->node);
     block->acquired = false;
@@ -56,18 +62,20 @@ static usize get_num_cached_blocks() {
 }
 
 // see `cache.h`.
-static Block* cache_acquire(usize block_no) {
+static Block *cache_acquire(usize block_no)
+{
     // TODO
     return 0;
 }
 
 // see `cache.h`.
-static void cache_release(Block* block) {
-    // TODO
+static void cache_release(Block *block)
+{
 }
 
 // initialize block cache.
-void init_bcache(const SuperBlock* _sblock, const BlockDevice* _device) {
+void init_bcache(const SuperBlock *_sblock, const BlockDevice *_device)
+{
     sblock = _sblock;
     device = _device;
 
@@ -75,29 +83,34 @@ void init_bcache(const SuperBlock* _sblock, const BlockDevice* _device) {
 }
 
 // see `cache.h`.
-static void cache_begin_op(OpContext* ctx) {
+static void cache_begin_op(OpContext *ctx)
+{
     // TODO
 }
 
 // see `cache.h`.
-static void cache_sync(OpContext* ctx, Block* block) {
+static void cache_sync(OpContext *ctx, Block *block)
+{
     // TODO
 }
 
 // see `cache.h`.
-static void cache_end_op(OpContext* ctx) {
+static void cache_end_op(OpContext *ctx)
+{
     // TODO
 }
 
 // see `cache.h`.
 // hint: you can use `cache_acquire`/`cache_sync` to read/write blocks.
-static usize cache_alloc(OpContext* ctx) {
+static usize cache_alloc(OpContext *ctx)
+{
     // TODO
 }
 
 // see `cache.h`.
 // hint: you can use `cache_acquire`/`cache_sync` to read/write blocks.
-static void cache_free(OpContext* ctx, usize block_no) {
+static void cache_free(OpContext *ctx, usize block_no)
+{
     // TODO
 }
 
