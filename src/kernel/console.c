9ca8b5ec3508f20dbc995b87b3be5c0e124dc890
qsliu <qsliu2017@outlook.com>
2023-01-17 00:04:54 +0800

    console

--- src/kernel/console.c
+++ src/kernel/console.c
@@ -1,8 +1,11 @@
+#include "common/defines.h"
+#include "common/spinlock.h"
 #include <aarch64/intrinsic.h>
 #include <driver/uart.h>
 #include <kernel/console.h>
 #include <kernel/init.h>
 #include <kernel/sched.h>
+#include <stdlib.h>
 #define INPUT_BUF 128
 struct {
     char buf[INPUT_BUF];
@@ -10,16 +13,83 @@ struct {
     usize w; // Write index
     usize e; // Edit index
 } input;
+static inline char *chptr(usize index) { return &input.buf[index % INPUT_BUF]; }
+#define BACKSPACE 0x08
 #define C(x) ((x) - '@') // Control-x
 
+static void console_putc(char c) {
+    if (c == BACKSPACE) {
+        uart_put_char('\b');
+        uart_put_char(' ');
+        uart_put_char('\b');
+        return;
+    }
+    uart_put_char(c);
+}
+
 isize console_write(Inode *ip, char *buf, isize n) {
-    // TODO
+    inodes.lock(ip);
+
+    isize i;
+    for (i = 0; i < n; i++) {
+        if (buf[i] == 0) {
+            break;
+        }
+        console_putc(buf[i]);
+    }
+
+    inodes.unlock(ip);
+
+    return i;
 }
 
 isize console_read(Inode *ip, char *dst, isize n) {
-    // TODO
+    inodes.lock(ip);
+
+    isize i;
+
+    for (i = 0; i < n && input.r < input.w; i++) {
+        if (!dst[i]) {
+            break;
+        }
+        dst[i] = *chptr(input.r++);
+    }
+
+    inodes.unlock(ip);
+
+    return i;
 }
 
 void console_intr(char (*getc)()) {
-    // TODO
+    char c = getc();
+    switch (c) {
+    case BACKSPACE:
+        if (input.w < input.e) {
+            input.e--;
+            console_putc(BACKSPACE);
+        }
+        break;
+    case C('U'): /* Delete a line */
+        while (input.w < input.e && *chptr(input.e - 1) != '\n') {
+            input.e--;
+            console_putc(BACKSPACE);
+        }
+        break;
+    case C('C'): /* Kill */
+        exit(-1);
+        break;
+    case C('D'): /* EOF */
+        if (input.e - input.r < INPUT_BUF) {
+            console_putc(0);
+            *chptr(input.e++) = 0;
+            input.w = input.e;
+        }
+        break;
+    default:
+        if (input.e - input.r < INPUT_BUF) {
+            console_putc(c);
+            *chptr(input.e++) = c;
+        }
+        input.w++;
+    }
 }
