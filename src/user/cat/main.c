c44a74a8019b8924ec4adeb1517a57db2b72cc50
qsliu <qsliu2017@outlook.com>
2023-01-20 10:14:00 +0800

    cat

--- src/user/cat/main.c
+++ src/user/cat/main.c
@@ -6,6 +6,24 @@
 #include <unistd.h>
 
 int main(int argc, char *argv[]) {
-    // TODO
+#define BUF_SZ 512
+    char buf[BUF_SZ];
+    char *s;
+
+    if (argc == 1 || (argc == 2 && !strcmp(argv[1], "-"))) {
+        while ((s = fgets(buf, BUF_SZ, stdin)))
+            printf("%s", s);
+        return 0;
+    }
+
+    for (int i = 1; i < argc; i++) {
+        int fd = open(argv[i], O_RDONLY);
+        for (int n; (n = read(fd, buf, BUF_SZ - 1));) {
+            buf[n] = 0;
+            printf("%s", buf);
+        }
+
+        close(fd);
+    }
     return 0;
 }
